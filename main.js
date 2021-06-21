8bc59481b04c4feca27c456216dbceafdae53a60
Adam Mann <aemann2@gmail.com>
2021-06-21 12:13:37 -0700

    finished range slider nightmare

--- main.js
+++ main.js
@@ -40,3 +40,36 @@ toggle.addEventListener('change', (e) => {
 		discount.innerHTML = '25% discount';
 	}
 })
+
+// Slider code taken from:
+// https://codepen.io/nosurprisethere/pen/KJxwQz
+
+// Setting color of our sliders
+const settings={
+  fill: 'hsl(174, 77%, 80%)',
+  background: 'hsl(224, 65%, 95%)'
+}
+
+// Find all our sliders
+const sliders = document.querySelectorAll('.range-slider');
+
+// Iterate through that list of sliders
+Array.prototype.forEach.call(sliders,(slider)=>{
+  // Look inside our slider for our input add an event listener
+//   ... the input inside addEventListener() is looking for the input action, we could change it to something like change
+  slider.querySelector('input').addEventListener('input', (event)=>{
+    applyFill(event.target);
+  });
+  // Don't wait for the listener, apply it now!
+  applyFill(slider.querySelector('input'));
+});
+
+// This function applies the fill to our sliders by using a linear gradient background
+function applyFill(slider) {
+  // Let's turn our value into a percentage to figure out how far it is in between the min and max of our input
+  const percentage = 100*(slider.value-slider.min)/(slider.max-slider.min);
+  // now we'll create a linear gradient that separates at the above point
+  // Our background color will change here
+  const bg = `linear-gradient(90deg, ${settings.fill} ${percentage}%, ${settings.background} ${percentage+0.1}%)`;
+  slider.style.background = bg;
+}
